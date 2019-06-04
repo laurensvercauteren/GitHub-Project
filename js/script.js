32d0033462de28be522d90aaa2687f6eda416d63
Laurens Vercauteren <laurensvercauteren@users.noreply.github.com>
2019-06-04 16:32:56 +0200

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 function unhide() {
-    var div = document.getElementByClassName('add');
+    var div = document.getElementsByClassName('add')[0];
     div.innerHTML += 'fag ';
     div.classList.remove("add");
 }
